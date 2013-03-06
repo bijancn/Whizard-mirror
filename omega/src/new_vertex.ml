38ae369b8eaca5fd92cd30041e038fb70171647b
Thorsten Ohl <ohl@physik.uni-wuerzburg.de>
2013-03-06 11:18:50 +0100

    Array based implementation is MUCH faster

--- omega/src/new_vertex.ml
+++ omega/src/new_vertex.ml
@@ -1,8 +1,20 @@
 module type Test =
   sig
     val suite : OUnit.test
+    val time : unit -> unit
   end
-    
+
+let time f x =
+  let start = Sys.time () in
+  let f_x = f x in
+  let stop = Sys.time () in
+  (f_x, stop -. start)
+  
+let print_time msg f x =
+  let f_x, seconds = time f x in
+  Printf.printf "%s took %10.2f ms\n" msg (seconds *. 1000.);
+  f_x
+  
 module type Partial =
   sig
     type domain
@@ -56,19 +68,24 @@ module Partial_Test : Test=
     let suite =
       "Partial" >:::
 	[suite_apply]
-	
+
+    let time () =
+      ()
+
   end
 
 module type Permutation =
   sig
     type t
     val of_list : int list -> t
+    val of_array : int array -> t
     val inverse : t -> t
     val compose : t -> t -> t
-    val apply : t -> 'a list -> 'a list
+    val list : t -> 'a list -> 'a list
+    val array : t -> 'a array -> 'a array
   end
 
-module Permutation : Permutation =
+module Permutation_List : Permutation =
   struct
 
     type t = int list
@@ -79,31 +96,122 @@ module Permutation : Permutation =
       else
 	p
 
+    let of_array p =
+      try
+	of_list (Array.to_list p)
+      with 
+      | Invalid_argument "Permutation.of_list" ->
+	invalid_arg "Permutation.of_array"
+
     let inverse p = snd (ThoList.ariadne_sort p)
 
-    let apply pl l =
+    let list p l =
       List.map snd
 	(List.sort compare
 	   (try
-	      List.rev_map2 (fun p a -> (p, a)) pl l
+	      List.rev_map2 (fun i x -> (i, x)) p l
 	    with
 	    | Invalid_argument "List.rev_map2" ->
-	      invalid_arg "Permutation.apply: length mismatch"))
+	      invalid_arg "Permutation.list: length mismatch"))
+
+    let array p a =
+      try
+	Array.of_list (list p (Array.to_list a))
+      with 
+      | Invalid_argument "Permutation.list: length mismatch" ->
+	invalid_arg "Permutation.array: length mismatch"
 
 (* Probably not optimal (or really inefficient), but correct by
    associativity. *)
 
     let compose p q =
-      apply (inverse q) p
+      list (inverse q) p
 
-(* TODO: steal BatRandom.shuffle to implement random permutations. *)
   end
 
-module Permutation_Test : Test =
+module Permutation_Array : Permutation =
+  struct
+
+    type t = int array
+
+    let of_list p =
+      if List.sort compare p <> (ThoList.range 0 (List.length p - 1)) then
+	invalid_arg "Permutation.of_list"
+      else
+	Array.of_list p
+
+    let of_array p =
+      try
+	of_list (Array.to_list p)
+      with 
+      | Invalid_argument "Permutation.of_list" ->
+	invalid_arg "Permutation.of_array"
+      
+      let inverse p =
+      let len_p = Array.length p in
+      let p' = Array.make len_p p.(0) in
+      for i = 0 to pred len_p do
+	p'.(p.(i)) <- i
+      done;
+      p'
+
+    let array p a =
+      let len_a = Array.length a
+      and len_p = Array.length p in
+      if len_a <> len_p then
+	invalid_arg "Permutation.array: length mismatch";
+      let a' = Array.make len_a a.(0) in
+      for i = 0 to pred len_a do
+	a'.(p.(i)) <- a.(i)
+      done;
+      a'
+
+    let list p l =
+      try
+	Array.to_list (array p (Array.of_list l))
+      with 
+      | Invalid_argument "Permutation.array: length mismatch" ->
+	invalid_arg "Permutation.list: length mismatch"
+
+    let compose p q =
+      array (inverse q) p
+
+  end
+
+module Permutation = Permutation_Array
+
+(* To shuffle an array a of n elements (indices 0..n-1):
+
+     for i from n − 1 downto 1 do
+          j ← random integer with 0 ≤ j ≤ i
+          exchange a[j] and a[i]
+
+   To initialize an array a of n elements to a randomly shuffled copy
+   of source, both 0-based: 
+
+     a[0] ← source[0]
+     for i from 1 to n − 1 do
+         j ← random integer with 0 ≤ j ≤ i
+         a[i] ← a[j]
+         a[j] ← source[i] *)
+
+let shuffle l =
+  let a = Array.of_list l in
+  for n = Array.length a - 1 downto 1 do
+    let k = Random.int (succ n) in
+    if k <> n then
+      let tmp  = Array.get a n in
+      Array.set a n (Array.get a k);
+      Array.set a k tmp
+  done;
+  Array.to_list a
+
+module Permutation_Test (Permutation : Permutation) : Test =
   struct
 
     open OUnit
-    open Permutation
+    module P = Permutation
+    open P
 
     let of_list_overlap =
       "overlap" >::
@@ -135,15 +243,15 @@ module Permutation_Test : Test =
       "invalid/lengths" >::
 	(fun () ->
 	  assert_raises
-	    (Invalid_argument "Permutation.apply: length mismatch")
+	    (Invalid_argument "Permutation.list: length mismatch")
 	    (fun () ->
-	      apply (of_list [0;1;2;3;4]) [0;1;2;3]))
+	      list (of_list [0;1;2;3;4]) [0;1;2;3]))
 
     let apply_ok =
       "ok" >::
 	(fun () ->
 	  assert_equal [2;0;1;3;5;4]
-	    (apply (of_list [1;2;0;3;5;4]) [0;1;2;3;4;5]))
+	    (list (of_list [1;2;0;3;5;4]) [0;1;2;3;4;5]))
 
     let suite_apply =
       "apply" >:::
@@ -153,9 +261,9 @@ module Permutation_Test : Test =
     let inverse_ok =
       "ok" >::
 	(fun () ->
-	  let p = of_list [1;2;0;3;5;4]
-	  and l = [0;1;2;3;4;5] in
-	  assert_equal l (apply (inverse p) (apply p l)))
+	  let l = shuffle (ThoList.range 0 1000) in
+	  let p = of_list (shuffle l) in
+	  assert_equal l (list (inverse p) (list p l)))
 
     let suite_inverse =
       "inverse" >:::
@@ -164,16 +272,18 @@ module Permutation_Test : Test =
     let compose_ok =
       "ok" >::
 	(fun () ->
-	  let p = of_list [1;2;0]
-	  and q = of_list [0;2;1]
-	  and l = [0;1;2] in
-	  assert_equal (apply p (apply q l)) (apply (compose p q) l))
+	  let id = ThoList.range 0 1000 in
+	  let p = of_list (shuffle id)
+	  and q = of_list (shuffle id)
+	  and l = id in
+	  assert_equal (list p (list q l)) (list (compose p q) l))
 		
     let compose_inverse_ok =
       "inverse/ok" >::
 	(fun () ->
-	  let p = of_list [1;2;0]
-	  and q = of_list [0;2;1] in
+	  let id = ThoList.range 0 1000 in
+	  let p = of_list (shuffle id)
+	  and q = of_list (shuffle id) in
 	  assert_equal
 	    (compose (inverse p) (inverse q))
 	    (inverse (compose q p)))
@@ -189,6 +299,25 @@ module Permutation_Test : Test =
 	 suite_apply;
 	 suite_inverse;
 	 suite_compose]
+
+    let repeat repetitions size =
+      let id = ThoList.range 0 size in
+      let p = of_list (shuffle id)
+      and l = shuffle id in
+      print_time (Printf.sprintf "reps=%d, len=%d" repetitions size)
+	(fun () ->
+	  for i = 1 to repetitions do
+	    Permutation.list p l
+	  done)
+	()
+      
+    let time () =
+      repeat 100000 10;
+      repeat 10000 100;
+      repeat 1000 1000;
+      repeat 100 10000;
+      repeat 10 100000;
+      ()
 
   end
 
@@ -231,18 +360,34 @@ let vector_current =
     lorentz = [ (Integer 1, [Gamma (0, 2)]) ];
     color = [ (1, [T (1, 0, 2)])] }
 
+module Permutation_List_Test = Permutation_Test (Permutation_List)
+module Permutation_Array_Test = Permutation_Test (Permutation_Array)
+
 let () =
   let my_name = Sys.argv.(0) in
-  let verbose = ref false
+  let test = ref false
+  and timing = ref false
+  and verbose = ref false
   and usage = "usage: " ^ my_name ^ " ..." in
   Arg.parse
-    [ ("-verbose", Arg.Set verbose, "") ]
+    [ ("-test", Arg.Set test, "");
+      ("-timing", Arg.Set timing, "");
+      ("-verbose", Arg.Set verbose, "") ]
     (fun s -> raise (Arg.Bad s))
     usage;
-  let suite =
-    OUnit.(>:::) "All" 
-      [Partial_Test.suite;
-       Permutation_Test.suite] in
-  let _ =
-    OUnit.run_test_tt ~verbose:!verbose suite in
+  if !test then begin
+    let suite =
+      OUnit.(>:::) "All" 
+	[Partial_Test.suite;
+	 Permutation_List_Test.suite;
+	 Permutation_Array_Test.suite] in
+    ignore (OUnit.run_test_tt ~verbose:!verbose suite)
+  end;
+  if !timing then begin
+    Partial_Test.time ();
+    print_endline "List based:";
+    Permutation_List_Test.time ();
+    print_endline "Array based:";
+    Permutation_Array_Test.time ()
+  end;
   ()
