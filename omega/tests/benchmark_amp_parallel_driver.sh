59e47f26211b71f4aa63339e87de6fd5919b7ad0
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2014-11-11 12:00:37 +0100

    Last changes on the test suite

--- omega/tests/benchmark_amp_parallel_driver.sh
+++ omega/tests/benchmark_amp_parallel_driver.sh
@@ -72,11 +72,13 @@ cat <<EOF
   type(vm_t), dimension(:), allocatable :: vms
   type(vm_t) :: vm
   integer, parameter :: SEED = 42
+  integer, dimension(:), allocatable :: helicity
   integer :: max_threads, num_threads, i_threads, i, nout
   real(double) :: wtime_start
   real(double), dimension(:), allocatable :: elapsed_ps, elapsed_amp
   type(string_t) :: bytecode_file
   real(kind=default), dimension(:,:,:), allocatable :: p
+  integer :: remainder
 
   write(*, "(A)") '#All times in ms/phasespacepoint. All colors and helicities computed.'
   write(*, "(A)") '#Either the phase space is parallelized or the computation of a single amplitude'
@@ -85,6 +87,7 @@ cat <<EOF
   allocate (elapsed_ps(max_threads))
   allocate (elapsed_amp(max_threads))
   call omp_set_dynamic (.false.)
+  call omp_set_nested (.false.)
 EOF
 
 for model in $models; do
@@ -106,12 +109,18 @@ cat <<EOF
   call initialize_vm_$model (vm, bytecode_file)
   nout = vm%number_particles_out ()
   allocate (p(0:3, nout+2, max_threads))
+  allocate (helicity(nout+2))
+  helicity = (/(1, i = 1, nout+2)/)
   do i = 1, max_threads
-    call beams (${roots}.0_default, 0.0_default, 0.0_default, p(:,1,i), p(:,2,i))
+     call beams (${roots}.0_default, 0.0_default, 0.0_default, p(:,1,i), p(:,2,i))
   end do
 
   do num_threads = 1, max_threads
      call omp_set_num_threads (num_threads)
+     ! Initialize thread pool
+     !\$omp parallel
+     if(omp_get_thread_num() .eq. 999999) stop 1
+     !\$omp end parallel
      allocate (vms(num_threads))
      do i_threads = 1, num_threads
         call initialize_vm_$model (vms(i_threads), bytecode_file)
@@ -122,24 +131,32 @@ cat <<EOF
      do i_threads = 1, num_threads
         do i = 1, $n / num_threads
            call massless_isotropic_decay (${roots}.0_default, p(:,3:, i_threads))
-           call vms(i_threads)%new_event (p(:,:,i_threads))
+           call vms(i_threads)%run (p(:,:,i_threads), helicity=helicity)
         end do
      end do
      !\$omp end parallel do
+     remainder = $n - ($n / num_threads) * num_threads
+     if (remainder > 0) then
+        !\$omp parallel do
+        do i_threads = 1, remainder
+           call massless_isotropic_decay (${roots}.0_default, p(:,3:, i_threads))
+           call vms(i_threads)%run (p(:,:,i_threads), helicity=helicity)
+        end do
+        !\$omp end parallel do
+     end if
      elapsed_ps(num_threads) = omp_get_wtime () - wtime_start
-     deallocate(vms)
+     deallocate (vms)
 
      wtime_start = omp_get_wtime ()
      do i = 1, $n
-        call massless_isotropic_decay (${roots}.0_default, p(:,3:, 1))
-        call vm%new_event (p(:,:,1))
-        !print *, vm%color_sum (1, 1)
-        !print *, i
+        call massless_isotropic_decay (${roots}.0_default, p(:,3:,1))
+        call vm%run (p(:,:,1), helicity=helicity)
      end do
      elapsed_amp(num_threads) = omp_get_wtime () - wtime_start
   end do
 
   deallocate(p)
+  deallocate(helicity)
   elapsed_amp = elapsed_amp * 1000 / $n
   elapsed_ps = elapsed_ps * 1000 / $n
   do num_threads = 1, max_threads
