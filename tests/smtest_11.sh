4b6eec89827aa1d4a692f2303eeb9e4a63831563
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-06-12 14:49:03 +0200

    Implement Mikaels Patches Part I
    Ensuring the HEPEVT order is now optional and off per default.

--- tests/smtest_11.sh
+++ tests/smtest_11.sh
@@ -4,8 +4,10 @@ echo "Running script $0"
 script=`basename @script@`
 if test -f OCAML_FLAG; then
     ./run_whizard.sh @script@ --no-logging
-    echo "Contents of ${script}_tt.mokka.evt:" >> $script.log
-    cat ${script}_tt.mokka.evt >> $script.log
+    echo "Contents of ${script}a.mokka.evt:" >> $script.log
+    cat ${script}a.mokka.evt >> $script.log
+    echo "Contents of ${script}b.mokka.evt:" >> $script.log
+    cat ${script}b.mokka.evt >> $script.log
     diff ref-output/$script.ref $script.log
 else
     echo "|=============================================================================|"
