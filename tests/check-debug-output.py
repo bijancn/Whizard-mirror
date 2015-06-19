b536cfeb3b5a9660f0641ef99275cf9fb42705b6
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-06-19 12:05:52 +0200

    Add first real shower test case.
    Also add flag for Python to allow python analysis script

---
+++ tests/check-debug-output.py
@@ -0,0 +1,25 @@
+import re, sys
+
+re_e = re.compile("p\(0:3\) = *([0-9]+\.[0-9]+)")
+energy = lambda line: float(re_e.search(line).group(1))
+filename = "e2E2_sm.debug"
+inc_energy = 0.0
+out_energy = 0.0
+line_no = 0
+valid = True
+
+print "Start file"
+print (3 * "{:<20s}").format("Line-Nr", "Incoming-Energy", "Outgoing-Energy")
+with open(filename, 'r') as infile:
+  for line in infile:
+    line_no += 1
+    if 'incoming momenta' in line:
+      inc_energy = energy(line)
+    elif 'outgoing momenta' in line:
+      out_energy = energy(line)
+      if abs(out_energy - inc_energy) / max([out_energy, inc_energy]) > 10.0**-4:
+        print ("{:<20d}" + 2 *"{:<20.10f}").format(line_no, inc_energy, out_energy)
+        valid = False
+print "End file"
+returncode = 0 if valid else 1
+sys.exit(returncode)
--- tests/extra_shower_1.sh
+++ tests/extra_shower_1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # TODO: (bcn 2015-06-18) description
 echo "Running script $0"
-if test -f OCAML_FLAG -a -f PYTHIA6_FLAG; then
+if test -f OCAML_FLAG -a -f PYTHIA6_FLAG -a -f PYTHON_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
     diff ref-output/$s.ref $s.log
