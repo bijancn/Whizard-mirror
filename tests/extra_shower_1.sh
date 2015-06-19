9c37dbec54d06738ac9889227a0ea70feeeff8e2
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-06-19 14:45:23 +0200

    Fumbling on extra_shower

--- tests/extra_shower_1.sh
+++ tests/extra_shower_1.sh
@@ -4,7 +4,7 @@ echo "Running script $0"
 if test -f OCAML_FLAG -a -f PYTHIA6_FLAG -a -f PYTHON_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
-    diff ref-output/$s.ref $s.log
+    ./check-debug-output.py $s
   else
     echo "|=============================================================================|"
     echo "No O'Mega matrix elements available and/or PYTHIA6 disabled, test skipped"
