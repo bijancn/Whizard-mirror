f061d41792dcee222c481464733b42d2629f5468
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-06-26 15:00:10 +0200

    The same for NLO and some more cleanup

--- tests/extended_tests/extracheck_combiner.sh
+++
@@ -1,9 +0,0 @@
-tests="$1"
-echo $tests
-echo
-for t in $1; do
-  result=`grep ":test-result:" $t | sed 's/:test-result://'`
-  test=$t
-  test=`echo $t | sed 's/.run.extra.trs//'`
-  echo '   ' $test '   ' $result
-done
