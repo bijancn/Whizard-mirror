449d081a3cf7c73ce2c53176eb7b3e3612fabbf8
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-06-26 15:30:48 +0200

    `extra_nlo_1` works and wants many cousins

--- tests/extended_tests_NLO/extra_nlo_1.sh
+++ tests/extended_tests_NLO/extra_nlo_1.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# TODO: (bcn 2015-06-18) description
+# time ~
 # TODO: (bcn 2015-06-18) why do we have no OPENLOOPS_FLAG ?
 echo "Running script $0"
 if test -f OCAML_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
-    diff ref-output/$s.ref $s.log
+    @python_bin@ @share_dir@/compare-integrals.py $s \
+      @share_dir@/extra_integration_results.dat &>> $s.run.log
   else
     echo "|=============================================================================|"
     echo "No O'Mega matrix elements available"
--- tests/extended_tests_shower/extra_shower_2.sh
+++ tests/extended_tests_shower/extra_shower_2.sh
@@ -4,7 +4,6 @@ echo "Running script $0"
 if test -f OCAML_FLAG -a -f PYTHIA6_FLAG -a -f PYTHON_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
-    rm -f $s.integration
     @python_bin@ @share_dir@/compare-integrals.py $s \
       @share_dir@/extra_integration_results.dat &>> $s.run.log
     @python_bin@ @share_dir@/compare-histograms.py $s &>> $s.run.log
