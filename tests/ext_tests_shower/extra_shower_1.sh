c852d253c5beae69da3a9ce42d71a1568df5be06
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-07-02 14:35:09 +0200

    Correctly capture exit codes in run scripts

--- tests/ext_tests_shower/extra_shower_1.sh
+++ tests/ext_tests_shower/extra_shower_1.sh
@@ -4,7 +4,8 @@ echo "Running script $0"
 if test -f OCAML_FLAG -a -f PYTHIA6_FLAG -a -f PYTHON_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
-    @python_bin@ @share_dir@/check-debug-output.py $s 1>&2 &>> $s.run.log
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+    @python_bin@ @share_dir@/check-debug-output.py $s &>> $s.run.log
   else
     echo "|=============================================================================|"
     echo "O'Mega, PYTHIA6 and/or Python disabled/not available, test skipped"
