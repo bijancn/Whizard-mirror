062105d38fa20ecaf3a20901141cd999e09028c6
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-07-10 15:57:27 +0200

    More NLO checks

--- share/tests/check-hepmc-weights.py
+++ share/tests/check-hepmc-weights.py
@@ -1,9 +1,20 @@
 import re, sys
+from math import sqrt
 
-re = re.compile("p\(0:3\) = *([0-9]+\.[0-9]+)")
+re_w = re.compile("E .* (-*\d*\.\d+e\S*)")
+re_nums = re.compile("C (\d+\.\d+e\S*) (\d*\.\d+e\S*)")
+def xsection(line):
+  try:
+    return float(re_nums.search(line).group(1)), float(re_nums.search(line).group(2))
+  except AttributeError:
+    print 'Could not find xsection in this line:'
+    print line
+    return 0.0
+
 def weight(line):
   try:
-    return float(re.search(line).group(1))
+    weight = float(re_w.search(line).group(1))
+    return weight
   except AttributeError:
     print 'Could not find number in this line:'
     print line
@@ -11,24 +22,47 @@ def weight(line):
 
 test = sys.argv[1]
 filename = test + '.hepmc'
-line_no = 0
 valid = True
+xsec = 0.0
+error = 0.0
+sum_weights = [0.0, 0.0, 0.0]
+NN = [0, 0, 0]
+line_no = 0
+region = 0
+regions = range(3)
 
 print 'Start file ' + filename
-print (4 * '{:<20s}').format('Line-Nr', 'BeamRemnant-Energy', 'Outgoing-Energy',
-    'BeamRemnant+Outgoing-Energy')
+with open(filename, 'r') as infile:
+  for line in infile:
+    if 'C ' in line:
+      xsec, error = xsection(line)
+      break
+  print 'Cross section is', xsec, '+-', error
 with open(filename, 'r') as infile:
   for line in infile:
     line_no += 1
-    if 'beam remnant momenta' in line:
-      br_energy = energy(line)
-    elif 'outgoing momenta' in line:
-      out_energy = energy(line)
-      if not nearly_equal(sqrts, out_energy + br_energy):
-        print ("{:<20d}" + 3 *"{:<20.10f}").format(line_no, br_energy,
-            out_energy, out_energy + br_energy)
-        valid = False
-print 'Number of lines', line_no
-print 'End file ' + filename
+    if 'E ' in line:
+      this_weight = weight(line)
+      #print 'line', line_no, this_weight
+      sum_weights[region] += this_weight
+      NN[region] += 1
+      region += 1
+      if region == 3:
+        region = 0
+  print 'HepMC'
+  for region in regions:
+    mean = sum_weights[region] / NN[region]
+    print 'region', region
+    print 'NN', NN[region]
+    print 'sum_weights', sum_weights[region]
+    print 'mean', mean
+    print 50 * '-'
+  print 'Overall'
+  mean = sum(sum_weights) / sum(NN)
+  print 'mean', mean
+  print 'abs(mean - xsec)', abs(mean-xsec)
+  valid = abs(mean - xsec) < 3 * error
+  print 'End file ' + filename
+
 returncode = 0 if valid else 1
 sys.exit(returncode)
--- tests/ext_tests_nlo/extra_nlo_tt.sh
+++ tests/ext_tests_nlo/extra_nlo_tt.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-# time ~
+# time ~9min
 echo "Running script $0"
 if test -f OCAML_FLAG -a -f OPENLOOPS_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
     @python_bin@ @share_dir@/compare-integrals.py $s \
       @share_dir@/extra_integration_results.dat &>> $s.run.log
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+    @python_bin@ @share_dir@/check-hepmc-weights.py $s &>> $s.run.log
   else
     echo "|=============================================================================|"
     echo "No O'Mega matrix elements available"
