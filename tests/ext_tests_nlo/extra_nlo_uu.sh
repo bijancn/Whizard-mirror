b683e618c0192e930c7ef36de472c8349542f1d5
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-07-14 18:38:53 +0200

    Add powheg test (not really working yet) and add alphas/pi test to ee_uu

--- tests/ext_tests_nlo/extra_nlo_uu.sh
+++ tests/ext_tests_nlo/extra_nlo_uu.sh
@@ -4,6 +4,7 @@ echo "Running script $0"
 if test -f OCAML_FLAG -a -f OPENLOOPS_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
     @python_bin@ @share_dir@/compare-integrals.py $s \
       @share_dir@/extra_integration_results.dat &>> $s.run.log
     rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
