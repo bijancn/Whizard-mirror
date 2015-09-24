99acfd43b555fbbe6917d83c0cf503840d0cb4e9
Chrisitan Weiss <christian.weiss@desy.de>
2015-09-24 17:08:27 +0200

    Update to r7269

--- circe2/tests/delta.sh
+++ circe2/tests/delta.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 ########################################################################
 if test -f OCAML_FLAG; then
-    exec ./test_wrapper.sh @name@ check_generation </dev/null >/dev/null 2>&1
+  if test ! -f QUAD_FLAG; then
+      exec ./test_wrapper.sh @name@ check_generation </dev/null >/dev/null 2>&1
+  else
+      echo "|=============================================================================|"
+      echo "Detected quadruple precision: test skipped for now"
+      exit 77
+  fi
 else
     echo "|=============================================================================|"
     echo "No OCaml for testing tools available, test skipped"
