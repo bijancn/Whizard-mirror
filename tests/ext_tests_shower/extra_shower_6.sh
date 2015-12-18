cc39c9d732fb08c1ea5d3e5fed95378ecde4eefa
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2015-12-18 18:29:01 +0100

    pythia_shower: Add missing particles from Pythia when there are multiple decay steps in Whizard and extend the test-suite to cover this. Also remove bogus error message.

--- tests/ext_tests_shower/extra_shower_6.sh
+++ tests/ext_tests_shower/extra_shower_6.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-# time ~2 min
+# time ~3 min
 echo "Running script $0"
 if test -f OCAML_FLAG -a -f PYTHIA6_FLAG -a -f PYTHON_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging
     rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-    @python_bin@ @share_dir@/check-debug-output-hadro.py ${s}_WHIZARD \
-      7000 1>&2 &>> $s.run.log
-    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-    @python_bin@ @share_dir@/check-debug-output-hadro.py ${s}_PYTHIA6 \
-      7000 1>&2 &>> $s.run.log
+    @python_bin@ @share_dir@/check-debug-output.py ${s} &>> $s.run.log
   else
     echo "|=============================================================================|"
     echo "O'Mega, PYTHIA6 and/or Python disabled/not available, test skipped"
