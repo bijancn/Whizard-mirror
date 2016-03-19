6570fa138f1cfe40e42ac5873329a0a9940012a2
Chrisitan Weiss <christian.weiss@desy.de>
2016-03-19 14:01:52 +0100

    Update to r7485

--- tests/functional_tests/tauola_1.sh
+++ tests/functional_tests/tauola_1.sh
@@ -8,7 +8,7 @@ if test -f OCAML_FLAG -a -f PYTHIA6_FLAG; then
     echo "STDHEP EV4 version:" >> $script.log
     ./stdhep_rd ${script}_p1.ev4.hep 1 >> ${script}.log
     mv ${script}.log ${script}.log.tmp
-    cat ${script}.log.tmp | sed -e 's/total blocks.*/total blocks: [...]/' -e 's/WHIZARD 2.*/WHIZARD [version]/' -e 's/date: .*/date: [...]/' -e 's/[ -][0-9].[0-9][0-9]e-0[5-9]/ 0.00e+00/g' > ${script}.log
+    cat ${script}.log.tmp | sed -e 's/total blocks.*/total blocks: [...]/' -e 's/WHIZARD 2.*/WHIZARD [version]/' -e 's/date: .*/date: [...]/' -e 's/[ -][0-9].[0-9][0-9]e-0[5-9]/ 0.00e+00/g' -e 's/[ -][0-9].[0-9][0-9]e-1[0-9]/ 0.00e+00/g' > ${script}.log
     diff ref-output/${script}.ref ${script}.log
 else
     echo "|=============================================================================|"
