75995b95a83341ff32898a896dabf6b7adac2e34
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-06-10 14:01:34 +0200

    Reset changes to NLO tests (somehow doesnt work anymore)

--- tests/functional_tests/nlo_4.sh
+++ tests/functional_tests/nlo_4.sh
@@ -3,12 +3,10 @@
 echo "Running script $0"
 if test -f OCAML_FLAG; then
     name=`basename @script@`
-    (time -p ( {
     ./run_whizard.sh @script@ --no-logging
     echo "Contents of ${name}_p1.debug:" >> $name.log
     cat ${name}_p1.debug >> $name.log
     diff ref-output/$name.ref $name.log
-    } 2>&3) ) 3>&2 2>$name.timing
 else
     echo "|=============================================================================|"
     echo "No O'Mega matrix elements available, test skipped"
