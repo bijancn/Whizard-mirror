bc436a4c83e2401f769cffebd6ccd81e7051c36c
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-06-16 16:31:26 +0200

    gui: remove integration and fix linebreaks

--- share/gui/src/tabs.integration.js
+++
@@ -1,6 +0,0 @@
-// [Appearance]
-// Selecting Tabs:Integration > Process
-$(document).on('click', '.process-entry', () => {
-  $('.process-entry').removeClass('active');
-  $(this).addClass('active');
-});
