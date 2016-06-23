43d9d57e9789977bce44e80e21065e2ce0d331d0
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-06-23 18:07:14 +0200

    Remove timings again

--- tests/functional_tests/user_prc_threshold_1.sh
+++ tests/functional_tests/user_prc_threshold_1.sh
@@ -3,11 +3,9 @@
 echo "Running script $0"
 if test -f OCAML_FLAG; then
     name=`basename @script@`
-    (time -p ( {
     rm -f SM_tt_threshold.grid
     ./run_whizard.sh @script@ --no-logging
     diff ref-output/$name.ref $name.log
-    } 2>&3) ) 3>&2 2>$name.timing
 else
     echo "|=============================================================================|"
     echo "No O'Mega matrix elements available, test skipped"
--- tests/functional_tests/user_prc_threshold_2.sh
+++ tests/functional_tests/user_prc_threshold_2.sh
@@ -3,13 +3,9 @@
 echo "Running script $0"
 if test -f OCAML_FLAG; then
     s=`basename @script@`
-    (time -p (
-      {
-        ./run_whizard.sh @script@ --no-logging
-        rc=$?; if [ $rc != 0 ]; then exit $rc; fi
-        diff ref-output/${s}.ref ${s}.log
-      } 2>&3
-    ) ) 3>&2 2>${s}.timing
+    ./run_whizard.sh @script@ --no-logging
+    rc=$?; if [ $rc != 0 ]; then exit $rc; fi
+    diff ref-output/${s}.ref ${s}.log
   else
     echo "|=============================================================================|"
     echo "No O'Mega matrix elements available, test skipped"
