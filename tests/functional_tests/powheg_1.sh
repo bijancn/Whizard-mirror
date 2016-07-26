4ae5ed8822e74e3f330a2c3f21793d27dd428c79
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2016-07-26 16:45:48 +0200

    Remove 2nd powheg grid in test and update refs
    Somehow all powheg references were affected by the last change. Maybe they
    weren't consistently using the set options. Should be right now though.

--- tests/functional_tests/powheg_1.sh
+++ tests/functional_tests/powheg_1.sh
@@ -4,6 +4,7 @@ echo "Running script $0"
 if test -f OCAML_FLAG; then
     name=`basename @script@`
     rm -f ${name}_p1_1000_powheg_grids.dat
+    rm -f ${name}_p2_1000_powheg_grids.dat
     ./run_whizard.sh @script@ --no-logging
     echo "Contents of ${name}_p1.debug:" >> $name.log
     cat ${name}_p1.debug >> $name.log
