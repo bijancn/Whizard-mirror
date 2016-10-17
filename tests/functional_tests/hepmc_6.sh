6b266a215ff25c16e02f078531329c4d5f01d8b5
Juergen <juergen.reuter@desy.de>
2016-10-17 22:04:22 +0200

    -- Repairing erroneous change in hepmc_6 test -- flavor_get_isospin had also wrong sign (giving inconsistent results    for all tests involving EWA)

--- tests/functional_tests/hepmc_6.sh
+++ tests/functional_tests/hepmc_6.sh
@@ -4,7 +4,7 @@ echo "Running script $0"
 if test -f OCAML_FLAG -a -f HEPMC_FLAG; then
     s=`basename @script@`
     ./run_whizard.sh @script@ --no-logging --model QED
-    echo "Output from running ${s}_rd:" >> ${s}.log
+    echo "Output from running ${s}_rd:" > ${s}.log
     LD_LIBRARY_PATH=@HEPMC_LD_LIBRARY_PATH@ ./${s}_rd >> ${s}.log
     diff ref-output/$s.ref ${s}.log
 else
